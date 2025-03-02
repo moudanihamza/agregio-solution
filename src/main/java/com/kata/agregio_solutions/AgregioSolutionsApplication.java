6084812a2a57b0f3c80fd949f66f624cdff42165
Hamza MOUDANI <moudanihamza@gmail.com>
2025-03-02 16:25:04 +0100

    init project

---
+++ src/main/java/com/kata/agregio_solutions/AgregioSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.kata.agregio_solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregioSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregioSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kata/agregio_solutions/AgregioSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.agregio_solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregioSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
